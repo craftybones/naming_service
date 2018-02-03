b64d09b549d5af7972af2a8dac93e17c5b514741
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2018-02-03 14:10:41 +0530

    Ability to search by email

--- app/models/intern.rb
+++ app/models/intern.rb
@@ -1,7 +1,11 @@
 class Intern < ApplicationRecord
   include PgSearch
 
-  pg_search_scope :search, :against => [:display_name, :first_name, :last_name, :batch]
-
   has_many :emails
+
+  pg_search_scope :search, :against => [:display_name, :first_name, :last_name, :batch],
+                  :associated_against => {
+                      :emails => :address
+                  }
+
 end
