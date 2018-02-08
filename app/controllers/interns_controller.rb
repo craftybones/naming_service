3188ed8842c7eeae7d5059bab9d194f0eea47a0b
Sayali Jagtap <jagtapsayali0304@gmail.com>
2018-02-08 15:23:55 +0530

    [#001] - Added tests for update and create functionality.

--- app/controllers/interns_controller.rb
+++ app/controllers/interns_controller.rb
@@ -30,7 +30,7 @@ class InternsController < ApplicationController
   def create
     @intern = Intern.new(intern_params)
     @intern.save
-    redirect_to @intern
+    redirect_to intern_path(@intern)
   end
 
   def destroy
--- spec/controllers/interns_controller_spec.rb
+++ spec/controllers/interns_controller_spec.rb
@@ -5,14 +5,12 @@ RSpec.describe InternsController, type: :controller do
   describe 'GET index' do
     it 'should render index template' do
       get :index
-
       expect(response).to render_template('interns/index')
     end
 
     it 'should give the list of interns' do
       intern = Intern.create
       get :index
-
       expect(assigns(:interns)).to eq([intern])
     end
   end
@@ -20,18 +18,16 @@ RSpec.describe InternsController, type: :controller do
   describe 'GET new' do
     it 'should render new template' do
       get :new
-
       expect(response).to render_template('interns/new')
     end
 
     it 'should build the new empty intern' do
       get :new
-
       expect(assigns(:intern)).to be_a_new(Intern)
     end
   end
 
-  describe 'GET an intern' do
+  describe 'GET show' do
 
     before(:each) do
       @actual_intern = double('Intern', display_name: 'Intern 1')
@@ -45,7 +41,6 @@ RSpec.describe InternsController, type: :controller do
 
     it 'should find the intern by given id' do
       get :show, params: {id: 1}
-
       expect(assigns(:intern)).to eq(@actual_intern)
     end
   end
@@ -64,24 +59,47 @@ RSpec.describe InternsController, type: :controller do
 
     it 'should find the intern by given id' do
       get :edit, params: {id: 1}
-
       expect(assigns(:intern)).to eq(@actual_intern)
     end
   end
 
   describe 'PUT update' do
-    it 'should redirect to the created intern' do
-      intern = double('Intern', id: 'id')
-      intern_attributes = {display_name: 'Intern 1'}
+    before(:each) do
+      @intern = double('Intern', id: 'id')
+      @intern_attributes = {display_name: 'Intern 1'}
 
-      expect(Intern).to receive(:find).and_return(intern)
-      allow_any_instance_of(InternsController).to receive(:intern_params).and_return(intern_attributes)
-      allow(intern).to receive(:update).with(intern_attributes).and_return(true)
+      expect(Intern).to receive(:find).and_return(@intern)
+      allow_any_instance_of(InternsController).to receive(:intern_params).and_return(@intern_attributes)
+    end
+
+    it 'should redirect to the created intern' do
+      expect(@intern).to receive(:update).with(@intern_attributes).and_return(true)
 
       post :update, params: {id: 'id', display_name: 'Intern 1'}
-
       expect(response).to redirect_to(intern_path)
     end
 
+    it 'should render edit view when updation fails' do
+      expect(@intern).to receive(:update).with(@intern_attributes).and_return(false)
+
+      post :update, params: {id: 'id', display_name: 'Intern 1'}
+      expect(response).to render_template('interns/edit')
+    end
   end
+
+  describe 'POST create' do
+    it 'should redirect to the all interns page after creation of an intern' do
+      intern = double('Intern', id: 'id')
+      intern_attributes = {display_name: 'Intern 1'}
+
+      allow_any_instance_of(InternsController).to receive(:intern_params).and_return(intern_attributes)
+      expect(Intern).to receive(:new).with(intern_attributes).and_return(intern)
+      expect(intern).to receive(:save)
+
+      post :create, params: {id: 'id', display_name: 'Intern 1'}
+
+      expect(response).to redirect_to(intern_path(intern))
+    end
+  end
+
 end
