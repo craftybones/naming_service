10b7c0cd34dea6ffda11d2f13ce7e670d96b747b
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2018-06-13 16:37:42 +0530

    Added bearer token authentication for apis

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,10 @@
 class ApiController < ActionController::API
-  # protect_from_forgery with: :null_session
 
-  # before_action :authenticate
+  before_action :authenticate
 
   def current_user
     token = bearer_token
-    User.find_by(:token => token)
+    User.find_by(:token => token) if token
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180613052138) do
+ActiveRecord::Schema.define(version: 20180613110435) do
 
   create_table "batches", force: :cascade do |t|
     t.string "name"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20180613052138) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
   end
 
 end
