d42735c7e6668b6188b28bb51f1db2e256ba882b
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2018-06-13 11:59:26 +0530

    Show navigation bar options or links only if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    User.find(session[:user_id]) if session[:user_id]
   end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  helper_method :logged_in?
 
   def authenticate
     redirect_to '/login' unless current_user
