0fdca16a695a7ca825bbb16f4ab6ff5df5428292
Sayali Jagtap <jagtapsayali0304@gmail.com>
2018-08-10 07:24:26 +0530

    psql in local env

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180717083412) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "batches", force: :cascade do |t|
     t.string "name"
     t.date "start_date"
